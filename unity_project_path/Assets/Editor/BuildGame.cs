7cff4040255eec01e139f1cafe3da09ba169f799
motoyinc <motoyinc@gmail.com>
2024-09-22 14:27:47 +0800

    Fix: 移除打包脚本里的Editor空间命名
    打包脚本里的Editor空间命名会导致批处理无法正常执行

--- unity_project_path/Assets/Editor/BuildGame.cs
+++ unity_project_path/Assets/Editor/BuildGame.cs
@@ -2,75 +2,74 @@
 using System;
 using UnityEngine;
 
-namespace Editor
+
+public class BuildProject
 {
-    public class BuildProject
+    [MenuItem("Tools/Build")]
+    public static void BuildGame()
     {
-        // [MenuItem("Tools/Build")]
-        public static void BuildGame()
+        // 传入批处理参数
+        string[] args = Environment.GetCommandLineArgs();
+        
+        // 初始化构建参数以及默认值
+        string[] scenes = { "Assets/Scenes/Terminal/TerminalScene.unity" };
+        string locationPathName = "Build/Windows/YourGame.exe";
+        BuildTarget buildTarget = BuildTarget.StandaloneWindows64;
+        BuildOptions buildOptions = BuildOptions.None;
+        for (int i = 0; i < args.Length; i++)
         {
-            // 传入批处理参数
-            string[] args = Environment.GetCommandLineArgs();
-            
-            // 初始化构建参数以及默认值
-            string[] scenes = { "Assets/Scenes/Terminal/TerminalScene.unity" };
-            string locationPathName = "Build/Windows/YourGame.exe";
-            BuildTarget buildTarget = BuildTarget.StandaloneWindows64;
-            BuildOptions buildOptions = BuildOptions.None;
-            for (int i = 0; i < args.Length; i++)
+            switch (args[i])
             {
-                switch (args[i])
-                {
-                    case"-scenes":
-                        if (i + 1 < args.Length)
-                        {
-                            scenes = args[i + 1].Split(',');
-                            Debug.Log("参数 -scenes:"+ scenes[0]);
-                        }
-                        break;
-                    case"-locationPathName":
-                        if (i + 1 < args.Length)
-                        {
-                            locationPathName = args[i + 1];
-                            Debug.Log("参数 -locationPathName:"+ locationPathName);
-                            
-                        }
-                            
-                        break;
-                    case"-buildTarget":
-                        if (i + 1 < args.Length)
-                        {
-                            Enum.TryParse(args[i + 1], out buildTarget);
-                            if (buildTarget == BuildTarget.StandaloneWindows64)
-                                Debug.Log("参数 -buildTarget: True");
-                            else
-                                Debug.Log("参数 -buildTarget: False");
-                            
-                        }
-                        break;
-                    case"-buildOptions":
-                        if (i + 1 < args.Length)
-                        {
-                            Enum.TryParse(args[i + 1], out buildOptions);
-                            if (buildOptions ==  BuildOptions.None)
-                                Debug.Log("参数 -buildOptions: True");
-                            else
-                                Debug.Log("参数 -buildOptions: False");
-                        }
-                        break;
-                }
+                case"-scenes":
+                    if (i + 1 < args.Length)
+                    {
+                        scenes = args[i + 1].Split(',');
+                        Debug.Log("参数 -scenes:"+ scenes[0]);
+                    }
+                    break;
+                case"-locationPathName":
+                    if (i + 1 < args.Length)
+                    {
+                        locationPathName = args[i + 1];
+                        Debug.Log("参数 -locationPathName:"+ locationPathName);
+                        
+                    }
+                        
+                    break;
+                case"-buildTarget":
+                    if (i + 1 < args.Length)
+                    {
+                        Enum.TryParse(args[i + 1], out buildTarget);
+                        if (buildTarget == BuildTarget.StandaloneWindows64)
+                            Debug.Log("参数 -buildTarget: True");
+                        else
+                            Debug.Log("参数 -buildTarget: False");
+                        
+                    }
+                    break;
+                case"-buildOptions":
+                    if (i + 1 < args.Length)
+                    {
+                        Enum.TryParse(args[i + 1], out buildOptions);
+                        if (buildOptions ==  BuildOptions.None)
+                            Debug.Log("参数 -buildOptions: True");
+                        else
+                            Debug.Log("参数 -buildOptions: False");
+                    }
+                    break;
             }
-            
-            // 构建打包参数
-            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
-            buildPlayerOptions.scenes = scenes;
-            buildPlayerOptions.locationPathName = locationPathName;
-            buildPlayerOptions.target = buildTarget;
-            buildPlayerOptions.options = buildOptions;
-            
-            // 调用Unity的打包函数
-            BuildPipeline.BuildPlayer(buildPlayerOptions);
-            
         }
+        
+        // 构建打包参数
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = locationPathName;
+        buildPlayerOptions.target = buildTarget;
+        buildPlayerOptions.options = buildOptions;
+        
+        // 调用Unity的打包函数
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        
     }
 }
+
--- unity_project_path/Build_Win.bat
+++ unity_project_path/Build_Win.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Unity编辑器路径
-set UNITY_PATH="D:\UnityEditor\6000.0.19f1\Editor\Unity.exe"
+set UNITY_PATH="C:\Program Files\Unity\Hub\Editor\6000.0.19f1\Editor\Unity.exe"
 
 :: 设置项目路径为当前文件夹
 set PROJECT_PATH=%~dp0
@@ -17,7 +17,7 @@ set LOG_PATH=.\Logs\build.log
 set BUILD_TARGET="StandaloneWindows64"
 
 :: 设置输出路径
-set LOCATION_PATH_NAME=.\Build\Windows2\newGame.exe
+set LOCATION_PATH_NAME=.\Build\Windows\newGame.exe
 
 :: 参与构建的场景
 set SCENES="Assets\Scenes\Terminal\TerminalScene.unity"
@@ -26,6 +26,6 @@ set SCENES="Assets\Scenes\Terminal\TerminalScene.unity"
 set BUILD_OPTIONS="None"
 
 :: %UNITY_PATH% -batchmode -quit -projectPath %PROJECT_PATH% -executeMethod %EXECUTE_METHOD% -buildTarget %BUILD_TARGET% -logFile %LOG_PATH% -locationPathName %LOCATION_PATH_NAME% -scenes %SCENES% -buildOptions "None"
-%UNITY_PATH% -batchmode -quit -projectPath %PROJECT_PATH% -executeMethod %EXECUTE_METHOD% -logFile %LOG_PATH% -buildTarget %BUILD_TARGET% -locationPathName %LOCATION_PATH_NAME% -scenes %SCENES% -buildOptions %BUILD_OPTIONS%
+%UNITY_PATH% -batchmode -nographics -quit -projectPath %PROJECT_PATH% -executeMethod %EXECUTE_METHOD% -logFile %LOG_PATH% -buildTarget %BUILD_TARGET% -locationPathName %LOCATION_PATH_NAME% -scenes %SCENES% -buildOptions %BUILD_OPTIONS%
 echo Build completed
 pause
